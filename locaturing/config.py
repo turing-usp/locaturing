55b79fe9839026b715b663e65009a763194c6a56
WesPereira <wesleyalmeida@usp.br>
2021-05-11 19:23:28 -0300

    Feat: errors

--- locaturing/config.py
+++ locaturing/config.py
@@ -1,8 +1,6 @@
 class Config:
 
     TURING_AWS_BUCKET = 'turing-locaturing'
-    TURING_AWS_PUBLIC_KEY = 'AKIASICIBIYYI27ZPVV6' 
-    TURING_AWS_SECRET_KEY = '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' # NOQA
 
 
 cfg = Config()
--- locaturing/dataset.py
+++ locaturing/dataset.py
@@ -13,9 +13,7 @@ def get_dataframe(df_name: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: um DataFrame do pandas
     """
-    s3 = boto3.resource('s3',
-                        aws_access_key_id=cfg.TURING_AWS_PUBLIC_KEY, 
-                        aws_secret_access_key=cfg.TURING_AWS_SECRET_KEY)
+    s3 = boto3.resource('s3')
     obj = s3.Object(bucket_name=cfg.TURING_AWS_BUCKET, key=df_name)
     response = obj.get()
     df_raw = pd.read_csv(response['Body'])
